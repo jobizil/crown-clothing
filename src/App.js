819d56c2d56265997e17b99f57ba0174ccbd8c84
Job Ugbem <ugbemjob57@gmail.com>
2021-08-12 00:03:07 +0100

    Handle state change {signin and signout} on firebase

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { Component } from 'react'
 import { Switch, Route, BrowserRouter as Router } from 'react-router-dom'
 
 import './App.css'
@@ -6,19 +7,39 @@ import HomePage from './pages/homepage/home-page'
 import ShopPage from './pages/shop/shop-page'
 import SignInSignUpPage from './pages/sign-in-sign-up/sign-in-sign-up-page'
 
-function App() {
-  return (
-    <Router>
-      <div>
-        <Header />
-        <Switch>
-          <Route exact path='/' component={HomePage} />
-          <Route path='/shop' component={ShopPage} />
-          <Route path='/signin' component={SignInSignUpPage} />
-        </Switch>
-      </div>
-    </Router>
-  )
-}
+import { auth } from './firebase/firebase.utils'
 
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      currentUser: null,
+    }
+  }
+  // Handle User Signin and signout on firebase
+  unsubscribeFromAuth = null
+  componentDidMount() {
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
+      this.setState({ currentUser: user })
+      console.log(user)
+    })
+  }
+  componentWillUnmount() {
+    this.unsubscribeFromAuth()
+  }
+  render() {
+    return (
+      <Router>
+        <div>
+          <Header />
+          <Switch>
+            <Route exact path='/' component={HomePage} />
+            <Route path='/shop' component={ShopPage} />
+            <Route path='/signin' component={SignInSignUpPage} />
+          </Switch>
+        </div>
+      </Router>
+    )
+  }
+}
 export default App
