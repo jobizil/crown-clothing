dfc9aea9d806954db430eb0f6205656191834a93
Job Ugbem <ugbemjob57@gmail.com>
2021-08-18 11:05:48 +0100

    Added Checkout page, checkout item component and update cart to use reselect selsctors

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Component } from 'react'
-import { Switch, Route, Redirect } from 'react-router-dom'
+import { Switch, Route, Redirect, withRouter } from 'react-router-dom'
 import { connect } from 'react-redux'
+import { createStructuredSelector } from 'reselect'
 
 import './App.css'
 import Header from './components/header/header.component'
@@ -9,10 +10,12 @@ import Header from './components/header/header.component'
 import HomePage from './pages/homepage/home-page'
 import ShopPage from './pages/shop/shop-page'
 import SignInAndSignUpPage from './pages/sign-in-sign-up/sign-in-sign-up-page'
+import CheckoutPage from './pages/checkout/checkout.component'
 
 import { auth, createUserProfileDocument } from './firebase/firebase.utils'
 
 import { setCurrentUser } from './redux/user/user.actions'
+import { selectCurrentUser } from './redux/user/user.selector'
 class App extends Component {
   // Handle User Signin and signout on firebase
   unsubscribeFromAuth = null
@@ -49,6 +52,7 @@ class App extends Component {
         <Switch>
           <Route exact path='/' component={HomePage} />
           <Route path='/shop' component={ShopPage} />
+          <Route exact path='/checkout' component={CheckoutPage} />
           <Route
             exact
             path='/signin'
@@ -71,8 +75,8 @@ class App extends Component {
 https://react-redux.js.org/api/connect#mapdispatchtoprops-object--dispatch-ownprops--object 
 * null is passed into connect because the state is not actually being used by App Component.
 */
-const mapStateToProps = ({ user }) => ({
-  currentUser: user.currentUser,
+const mapStateToProps = createStructuredSelector({
+  currentUser: selectCurrentUser,
 })
 const mapDispatchToProps = dispatch => ({
   setCurrentUser: user => dispatch(setCurrentUser(user)),
--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -5,6 +5,9 @@ import { auth } from '../../firebase/firebase.utils'
 import { connect } from 'react-redux'
 import CartIcon from '../cart-icon/cart-icon.component'
 import CartDropdown from '../cart-dropdown/cart-dropdown.component'
+import { createStructuredSelector } from 'reselect'
+import { selectCurrentUser } from '../../redux/user/user.selector'
+import { selectHiddenCart } from '../../redux/cart/cart.selectors'
 const Header = ({ currentUser, hidden }) => {
   return (
     <div className='header'>
@@ -41,10 +44,12 @@ const Header = ({ currentUser, hidden }) => {
  * .user => user Value within the root Reducer
  * .currentUser=> user Reducer 
  https://react-redux.js.org/api/connect#mapstatetoprops-state-ownprops--object
+
+ * createStructuredSelector=> Helps combine multiple selector in a state and makes our code neater
  */
 
-const mapStateToProps = ({ user: { currentUser }, cart: { hidden } }) => {
-  return { currentUser, hidden }
-}
-
+const mapStateToProps = createStructuredSelector({
+  currentUser: selectCurrentUser,
+  hidden: selectHiddenCart,
+})
 export default connect(mapStateToProps)(Header)
