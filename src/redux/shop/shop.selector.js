5d83149f45350903b8c509d0fcdf5e56d441c063
Job Ugbem <ugbemjob57@gmail.com>
2021-08-23 11:55:16 +0100

    converted data to object for data noralization

--- src/redux/shop/shop.selector.js
+++ src/redux/shop/shop.selector.js
@@ -2,13 +2,6 @@ import memoize from 'lodash.memoize'
 
 import { createSelector } from 'reselect'
 
-const COLLECTION_ID_MAP = {
-  hats: 1,
-  sneakers: 2,
-  jackets: 3,
-  womens: 4,
-  mens: 5,
-}
 // Select the Shop
 
 const selectShop = state => state.shop
@@ -21,9 +14,8 @@ export const selectCollections = createSelector(
 /* Match url Params to shop collection */
 
 export const selectCollection = memoize(collectionUrlParam =>
-  createSelector(selectCollections, collections =>
-    collections.find(
-      collection => collection.id === COLLECTION_ID_MAP[collectionUrlParam]
-    )
+  createSelector(
+    selectCollections,
+    collections => collections[collectionUrlParam]
   )
 )
