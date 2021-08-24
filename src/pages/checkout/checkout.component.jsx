548217be95ab6dcef67c449f25df688a9d53ebd8
Job Ugbem <ugbemjob57@gmail.com>
2021-08-24 15:03:27 +0100

    Update favicon

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -36,7 +36,7 @@ const CheckoutPage = ({ cartItems, total }) => (
     <div className='test-warning'>
       *Please use the following test credit card for payments*
       <br />
-      4242 4242 4242 4242 - Exp: 01/23 - CVC: 123
+      5555 5555 5555 4444 - Exp: 01/23 - CVC: 123
     </div>
 
     <StripeCheckoutButton price={total} />
